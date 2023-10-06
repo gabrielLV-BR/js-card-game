23f24cd8f71c39ebdebd339c6d503dbddc8a84d4
Gabriel L. <gabriellovato15@gmail.com>
2023-10-05 22:35:30 -0300

    Removed unused code, begin working on Selection

--- src/ecs/component.ts
+++ src/ecs/component.ts
@@ -3,26 +3,3 @@ export abstract class Component {
         public readonly name: string
     ) {}
 }
-
-function _Component(target: any) {
-    const original = target;
-    
-    // Define a new constructor that performs validation
-    function newConstructor(...args: any[]) {
-        for (const arg of args) {
-        if (typeof arg !== 'string' || arg === '') {
-            throw new Error('Invalid argument: expected a non-empty string');
-        }
-        }
-    
-        // Call the original constructor with validated arguments
-        const instance = new original(...args);
-        return instance;
-    }
-    
-    // Set the prototype of the new constructor to the original prototype
-    newConstructor.prototype = original.prototype;
-    
-    // Return the new constructor
-    return newConstructor;
-}
