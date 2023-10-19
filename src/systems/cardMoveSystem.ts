dfabaa32dbbb401feb85591e8db6524470be79eb
Gabriel L. <gabriellovato15@gmail.com>
2023-10-19 12:29:36 -0300

    Progress on Card Movement System

--- src/systems/cardMoveSystem.ts
+++ src/systems/cardMoveSystem.ts
@@ -1,18 +1,36 @@
+import { Camera, Raycaster } from "three";
+import { CameraComponent } from "../components/cameraComponent";
 import { SelectedComponent } from "../components/selectedComponent";
 import { Query, System } from "../ecs/system";
+import { InputResource } from "../resources/inputResource";
+import { SceneResource } from "../resources/sceneResource";
 
 export class CardMoveSystem extends System {
 
-    cardQuery = new Query(SelectedComponent)
+    selectedQuery = new Query(SelectedComponent)
+    cameraQuery = new Query(CameraComponent)
+
+    inputResource = this.getResource(InputResource)
+    sceneResource = this.getResource(SceneResource)
+
+    raycast = new Raycaster()
 
     run(_delta: number): void {
-        for(const entity of this.query(this.cardQuery)) {
-            console.log("OI");
-            
-            const selectedComponent = entity.getComponent(SelectedComponent)
-            const object = selectedComponent.object
+        const mouseState = this.inputResource.mouse
+        const mouseDelta = mouseState.delta
 
-            object.scale.multiplyScalar(1.1)
+        for(const entity of this.query(this.cameraQuery)) {
+            const cameraComponent = entity.getComponent(CameraComponent)
+            this.moveObjects(cameraComponent)
+        }
+        
+    }
+
+    moveObjects({ camera }: CameraComponent) {
+        for(const entity of this.query(this.selectedQuery)) {
+
+            
+
         }
     }
 
